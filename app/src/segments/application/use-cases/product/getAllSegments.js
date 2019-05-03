5df3a065ad423b2b4c5b06aafe6d450f08b708ea
Liran Brimer <liran.brimer@visualdna.com>
2019-05-03 10:16:08 +0300

    fix tests

--- app/src/segments/application/use-cases/product/getAllSegments.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ segmentRepository }) => {
-
-  return async function getAllSegments ({ minHousehold = 0 } = {}) {
-    const segments = await segmentRepository.getAll()
-    const filteredSegments = segments.filter(s => s.getHouseholdCount() >= minHousehold)
-    return filteredSegments
-  }
-}
