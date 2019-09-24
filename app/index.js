02d816fcb1c03e224a01a98e1aa1e80e1fddca35
Liran <liranbri@gmail.com>
2019-09-24 14:05:02 +0300

    CodeSandBox.io

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
-const container = require('./src/segments/container')
+const container = require("./src/segments/container");
 
-process.on('unhandledRejection', error => {
-  console.error('unhandledRejection', error)
-})
+process.on("unhandledRejection", error => {
+  console.error("unhandledRejection", error);
+});
 
-const app = container.resolve('app')
+const app = container.resolve("app");
 
-app.start().catch((error) => {
-  console.error('failed to initiate container. error:', error)
-  app.logger.error(error.stack)
-  process.exit()
-})
+app.start().catch(error => {
+  console.error("1failed to initiate container. error:", error);
+  app.logger.error(error.stack);
+  process.exit();
+});
